41f6fc725f7202dd1cd18e30bc33021fe3feb045
Manish <manish.perumalla@cgi.com>
2019-08-05 09:46:33 +0530

    TrackDummyService Task

--- src/main/java/com/stackroute/trackservice/seeddata/CommandLineRunnerImpl.java
+++ src/main/java/com/stackroute/trackservice/seeddata/CommandLineRunnerImpl.java
@@ -22,13 +22,15 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
     int id;
     @Value("${name1}")
     String name;
+    @Value("${comments}")
+    String comment;
 
     @Autowired
     private Environment environment;
 
     @Override
     public void run(String... args) throws Exception {
-        Track track1=new Track(id,environment.getProperty("name"),"nice");
+        Track track1=new Track(id,environment.getProperty("name"),comment);
         trackRepository.save(track1);
         Track track2=new Track(3,name,"nice");
         trackRepository.save(track2);
